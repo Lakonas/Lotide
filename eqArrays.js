48784c88fc4c98c32144e70b742ca1f1a99d1fdc
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-18 19:52:58 -0500

    upload files to right directrory

--- eqArrays.js
+++ eqArrays.js
@@ -7,14 +7,16 @@ const assertEqual = function (actual,expected){
   
   };
 
-  const eqArrays = function (arr1[] ,arr2[]){
+  const eqArrays = function (arr1 ,arr2){
+    
+    if(arr1.length !== arr2.length) 
+      return false;
+    else{
+      for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+          return false;
+        } 
+      }return true;
+    }
+  };
 
-    if(arr1 === arr2){
-
-      return console.log("true");
-    } else return console.log("False");
-
-
-
-  }
-  
