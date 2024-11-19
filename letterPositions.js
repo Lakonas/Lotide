c6d622b4cfccb6396757d809970536ba1cea536c
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-19 01:46:19 -0500

    Update

--- letterPositions.js
+++ letterPositions.js
@@ -42,7 +42,7 @@ const letterPositions = function (sentence) {
       if(results[sentence[i]]){
         results[sentence[i]].push(i);
       } else {
-        results[sentence[i] = [i]];
+        results[sentence[i]] = [i];
       }
     }
   }
