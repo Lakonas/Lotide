7e4bc97d31cf884665ff1e722268693f603f78a1
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-26 01:42:34 -0500

    Test added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,19 @@
 
+
+
+
 const connect = require("./middle");
 const connect = require("./assertArraysEqual");
 
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
 middle([1, 2, 3, 4]); // => [2, 3]
 middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
