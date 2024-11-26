8d6d8b005b75d0d217920adf6ac8393d6524de68
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-25 23:22:18 -0500

    Obejcts tested

--- eqObjects.js
+++ eqObjects.js
@@ -6,22 +6,52 @@ const assertEqual = function (actual,expected){
   }else console.log(passed);
   
   };
-  const eqArrays = function (arr1 ,arr2){
-    
-    if(arr1.length !== arr2.length) 
+  const eqArrays = function(arr1, arr2) {
+  
+    if (arr1.length !== arr2.length) {
       return false;
-    else{
-      for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] !== arr2[i]){
+    }
+    
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+  const eqObjects = function(object1, object2) {
+    
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    
+    for (let key of keys1) {
+      
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
           return false;
-        } 
-      }return true;
+        }
+      } 
+      
+      else if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
+  
+    
+    return true;
   };
-  const eqObjects = function (object1, object2) {
 
-    if(object1 === object2){
-
-      return true;
-    }
-  };
+  const shirtObject = { color: "red", size: "medium" };
+  const anotherShirtObject = { size: "medium", color: "red" };
+  eqObjects(shirtObject, anotherShirtObject); // => true
+  
+  const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+  eqObjects(shirtObject, longSleeveShirtObject); // => false
+  assertEqual(shirtObject, anotherShirtObject );
